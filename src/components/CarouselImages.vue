da5cd81c653adc538e08d93d0209261993533346
ViorelChirila <viorelchirila21@yahoo.ro>
2022-12-07 13:30:00 +0200

    Carousel banner load random images v1.1
    Added search imput

--- src/components/CarouselImages.vue
+++ src/components/CarouselImages.vue
@@ -4,6 +4,9 @@ export default {
     return {
       photoData: null,
       items: [],
+      loaded: false,
+      loading: false,
+      searchInput: "",
     };
   },
   methods: {
@@ -25,6 +28,15 @@ export default {
           console.log(this.items);
         });
     },
+    onClick() {
+      this.loading = true;
+      console.log(this.searchInput);
+
+      setTimeout(() => {
+        this.loading = false;
+        this.loaded = true;
+      }, 2000);
+    },
   },
   mounted() {
     this.loadCarouselPhoto();
@@ -33,11 +45,54 @@ export default {
 </script>
 <template>
   <v-carousel cycle hide-delimiters :show-arrows="false">
-    <v-carousel-item
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item.src"
-      cover
-    ></v-carousel-item>
+    <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover>
+    </v-carousel-item>
+    <v-container class="stay" fluid>
+      <v-row class="justify-center">
+        <v-col cols="12" sm="6" md="4">
+          <v-card color="grey-darken-4" variant="tonal">
+            <v-card-title class="text-center">
+              We have the photo you need
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="justify-center">
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field
+            bg-color="grey-lighten-2"
+            :loading="loading"
+            density="compact"
+            variant="outlined"
+            label="Search images"
+            append-inner-icon="mdi-magnify"
+            single-line
+            hide-details
+            @click:append-inner="onClick"
+            @keyup.enter="onClick"
+            type="input"
+            v-model="searchInput"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-carousel>
 </template>
+<style>
+.stay {
+  position: absolute;
+  top: 25%;
+}
+
+.v-text-field {
+  transition: opacity 0.4s ease-in-out;
+}
+
+.v-text-field:not(.on-hover) {
+  opacity: 0.6;
+}
+
+.v-card-title {
+  color: rgb(141, 101, 138);
+}
+</style>
