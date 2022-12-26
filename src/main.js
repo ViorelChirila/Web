b0574b5caac3089126afeb44bd665dbc941f703f
ViorelChirila <viorelchirila21@yahoo.ro>
2022-12-26 21:46:44 +0200

    Added mitt

--- src/main.js
+++ src/main.js
@@ -1,25 +1,29 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from "vue";
+import mitt from "mitt";
+import App from "./App.vue";
 
 // Custom CSS
-import './assets/main.css';
+import "./assets/main.css";
 
 // Vuetify
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 // Plugins
-import { registerPlugins } from '@/plugins';
+import { registerPlugins } from "@/plugins";
 
+const emitter = mitt();
 const vuetify = createVuetify({
   components,
   directives,
 });
 
 // Register plugins
+
 const app = createApp(App);
+app.config.globalProperties.$emitter = emitter;
 registerPlugins(app);
 
-createApp(App).use(vuetify).mount('#app');
+createApp(App).use(vuetify).mount("#app");
