2f5f4009dfff074783b589538db088504e720e0a
ViorelChirila <viorelchirila21@yahoo.ro>
2022-12-27 23:31:45 +0200

    Added footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
+};
+</script>
+<template>
+  <v-footer class="bg-indigo-lighten-1 text-center d-flex flex-column">
+    <div>
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        :icon="icon"
+        variant="text"
+      ></v-btn>
+    </div>
+
+    <div class="pt-0">Get connected with us on social networks!</div>
+
+    <v-divider></v-divider>
+
+    <div>{{ new Date().getFullYear() }} — <strong>Vuetify</strong></div>
+  </v-footer>
+</template>
--- src/components/HomeApp.vue
+++ src/components/HomeApp.vue
@@ -1,8 +1,10 @@
 <script setup>
 import CarouselImages from "./CarouselImages.vue";
 import SampleCategorise from "./SampleCategorise.vue";
+import FooterComponent from "./FooterComponent.vue";
 </script>
 <template>
   <CarouselImages />
   <SampleCategorise />
+  <FooterComponent />
 </template>
