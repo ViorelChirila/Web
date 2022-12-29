b47bd4baad95949196f58aa27a9353264d270a7c
ViorelChirila <viorelchirila21@yahoo.ro>
2022-12-29 19:00:47 +0200

    Added search imput on search page

--- src/components/SearchPhotos.vue
+++ src/components/SearchPhotos.vue
@@ -4,11 +4,31 @@ export default {
   data() {
     return {
       searchValue: "",
+      loaded: false,
+      loading: false,
+      isertedSearchValue: false,
     };
   },
   methods: {
     takeSearchValue() {
-      this.searchValue = store.get();
+      if (store.get() !== "") {
+        this.searchValue = store.get();
+        this.isertedSearchValue = true;
+        store.insert("");
+      }
+    },
+    onClick() {
+      this.loading = true;
+      if (this.searchValue === "") {
+        this.isertedSearchValue = false;
+      } else {
+        this.isertedSearchValue = true;
+      }
+
+      setTimeout(() => {
+        this.loading = false;
+        this.loaded = true;
+      }, 2000);
     },
   },
   // mounted() {
@@ -26,12 +46,33 @@ export default {
 </script>
 
 <template>
-  <h1>Search {{ searchValue }}</h1>
-  <v-container>
+  <!-- <h1>Search {{ searchValue }}</h1> -->
+  <!-- <v-container>
     <v-row>
       <v-col v-for="n in 24" :key="n" cols="12" md="3" sm="6">
         <v-card height="200" color="grey-darken-4"></v-card>
       </v-col>
     </v-row>
-  </v-container>
+  </v-container> -->
+  <v-card class="mx-auto elevation-0 pt-5" max-width="800">
+    <v-card-text>
+      <v-text-field
+        :loading="loading"
+        density="compact"
+        variant="solo"
+        label="Search images"
+        append-inner-icon="mdi-magnify"
+        single-line
+        hide-details
+        @click:append-inner="onClick"
+        @keyup.enter="onClick"
+        type="input"
+        v-model="searchValue"
+        theme="dark"
+      ></v-text-field>
+    </v-card-text>
+  </v-card>
+  <h1 v-if="isertedSearchValue === false">
+    You haven't searched for anything yet!
+  </h1>
 </template>
